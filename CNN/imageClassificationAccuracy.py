f82d4ad2853d018b724a22cc30f91b1d201f3619
Kakaire Steven King <kakairestevenking@gmail.com>
2022-11-12 08:44:11 +0300

    Add files via upload

---
+++ CNN/imageClassificationAccuracy.py
@@ -0,0 +1,145 @@
+# import dependencies
+import keras
+from keras.datasets import cifar10
+from keras.preprocessing.image import ImageDataGenerator
+
+from keras.models import Sequential
+from keras.utils import np_utils
+from keras.layers import Dense, Activation, Flatten, Dropout, BatchNormalization, Conv2D, MaxPooling2D
+from keras.callbacks import ModelCheckpoint
+from keras import regularizers, optimizers
+
+# import numpy for math operations
+import numpy as np
+
+# import the matplotlib library to visualize results
+from matplotlib import pyplot
+
+# GET THE DATA READY FOR TRAINING
+# Downloads and splits the data
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+# Breaks the training set into training and validation sets
+(x_train, x_valid) = x_train[5000:], x_train[:5000]
+(y_train, y_valid) = y_train[5000:], y_train[:5000]
+
+print('x_train =', x_train.shape)
+print('x_valid =', x_valid.shape)
+print('x_test =', x_test.shape)
+
+# NORMALIZE THE DATA
+mean = np.mean(x_train, axis=(0, 1, 2, 3))
+std = np.std(x_train, axis=(0, 1, 2, 3))
+x_train = (x_train - mean) / (std + 1e-7)
+x_valid = (x_valid - mean) / (std + 1e-7)
+x_test = (x_test - mean) / (std + 1e-7)
+
+# ONE-HOT ENCODING
+num_classes = 10
+y_train = np_utils.to_categorical(y_train, num_classes)
+y_valid = np_utils.to_categorical(y_valid, num_classes)
+y_test = np_utils.to_categorical(y_test, num_classes)
+
+# DATA AUGMENTATION
+datagen = ImageDataGenerator(
+    rotation_range=15,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    horizontal_flip=True,
+    vertical_flip=False
+)
+
+datagen.fit(x_train)
+
+# BUILD THE MODEL
+# Number of hidden units variable
+base_hidden_units = 32
+
+# L2 regularization hyperparameter (ƛ)
+weight_decay = 1e-4
+model = Sequential()
+
+# CONV1
+model.add(Conv2D(base_hidden_units, kernel_size=3, padding='same',
+kernel_regularizer=regularizers.l2(weight_decay), input_shape = x_train.shape[1:]))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+
+# CONV2
+model.add(Conv2D(base_hidden_units, kernel_size=3, padding='same',
+kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+
+# POOL + Dropout
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.2))
+
+# CONV3
+model.add(Conv2D(base_hidden_units * 2, kernel_size=3, padding='same',
+kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+
+# CONV4
+model.add(Conv2D(base_hidden_units * 2, kernel_size=3, padding='same',
+kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+
+# POOL + Dropout
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.3))
+
+# CONV5
+model.add(Conv2D(base_hidden_units * 4, kernel_size=3, padding='same',
+kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+
+# CONV6
+model.add(Conv2D(base_hidden_units * 4, kernel_size=3, padding='same',
+kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+
+# POOL + Dropout
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.4))
+
+# FC7
+model.add(Flatten())
+model.add(Dense(10, activation='softmax'))
+model.summary()
+
+# TRAINING THE MODEL
+batch_size = 128
+epochs = 125
+checkpointer = ModelCheckpoint(filepath='model.100epochs.hdf5',
+    verbose=1,
+    save_best_only=True)
+optimizer = keras.optimizers.Adam(learning_rate=0.0001, decay=1e-6)
+
+model.compile(loss='categorical_crossentropy',
+    optimizer=optimizer,
+    metrics='accuracy')
+
+history = model.fit(datagen.flow(x_train, y_train, batch_size=batch_size),
+    callbacks=[checkpointer],
+    steps_per_epoch=x_train.shape[0] // batch_size,
+    epochs=epochs,
+    verbose=2,
+    validation_data=(x_valid, y_valid))
+
+# Evaluate the model
+scores = model.evaluate(
+    x_test, y_test, batch_size=128, verbose=1)
+print('\nTest results: %.3f loss: %.3f' % (scores[1]*100, scores[0]))
+ 
+# Plot the learning curves
+pyplot.plot(history.history['accuracy'], label='train')
+pyplot.plot(history.history['val_acc'], label='test')
+pyplot.legend()
+pyplot.show()
